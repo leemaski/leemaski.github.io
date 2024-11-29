84cba5a72c959067dded32ae1aa0b57ec8adcfed
Stanislav Kalgin <93548475+leemaski@users.noreply.github.com>
2024-11-30 02:34:05 +0300

    Update config.js

--- webapp/js/config.js
+++ webapp/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    API_URL: 'http://83.222.24.145:8000/api',  // URL вашего бэкенда
+    API_URL: 'https://telegram-shop-api.onrender.com',  // URL вашего бэкенда
     GITHUB_PAGES_URL: 'https://leemaski.github.io/webapp/'
 };
